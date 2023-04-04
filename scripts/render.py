64ebf6206a0c5d92fe217e1d1f6821bed9886480
Noah <noahyoda1@gmail.com>
2023-04-04 17:39:54 -0600

    objects now rotate dynamically around their center

--- scripts/render.py
+++ scripts/render.py
@@ -1,6 +1,6 @@
 import pygame, math
 import numpy as np
-import simulator
+import simulator as sim
 
 # window settings
 sc_width = 800
@@ -13,13 +13,13 @@ def project(angle_x, angle_y, vertices, center_pt, scale):
     rotation_matrix_y = [
         [1,0,0],
         [0, math.cos(angle_y), math.sin(angle_y)],
-        [0, -math.sin(angle_y), math.cos(angle_y)]
+        [0, -math.sin(angle_y), math.cos(angle_y)],
     ]
 
     rotation_matrix_x = [
         [math.cos(angle_x), 0, math.sin(angle_x)],
         [0,1,0],
-        [-math.sin(angle_x), 0, math.cos(angle_x)]
+        [-math.sin(angle_x), 0, math.cos(angle_x)],
     ]
 
     rotation_matrix = np.matmul(rotation_matrix_x, rotation_matrix_y)
@@ -38,11 +38,19 @@ def project(angle_x, angle_y, vertices, center_pt, scale):
     # project vertices to 2d coords
     for p in vertices:
         projected = np.matmul(proj_matrix, np.transpose(p))
-        x = center_pt['x'] + projected[0] * scale
-        y = center_pt['y'] + projected[1] * scale
-        proj_verts.append([x, y])
+        # apply scale
+        projected = [projected[i] * scale for i in range(len(projected))]
+        # apply center point
+        for i in range(0,len(projected),2):
+            projected[i] += center_pt['x']
+            projected[i+1] += center_pt['y']
+        proj_verts.append(projected)
     
     return proj_verts
+
+def draw_tube(start, end, radius):
+    # given a start point, end point, and radius draw a tube
+    pass
 
 def start_game():
     # init pygame settings
@@ -82,13 +90,37 @@ def start_game():
 
     angle_x = 0
     angle_y = 0
-    scale = 200
     running = True
     pause = False
     move_view = False
     m_pos = (0,0)
-    sim_pts = simulator.get_points()
-
+    sim_pts = sim.get_points()
+    # get center of points
+    ax = ay = az = 0
+    for pt in sim_pts:
+        ax += pt[0]
+        ay += pt[1]
+        az += pt[2]
+    ax /= len(sim_pts)
+    ay /= len(sim_pts)
+    az /= len(sim_pts)
+    # normalize points to be in range [-1,1]
+    max_x, max_y, max_z = 1,1,1
+    for pt in sim_pts:
+        if pt[0] > max_x:
+            max_x = pt[0]
+        if pt[1] > max_y:
+            max_y = pt[1]
+        if pt[2] > max_z:
+            max_z = pt[2]
+    m = max(max_x, max_y, max_z)
+    for pt in sim_pts:
+        pt[0] = (pt[0] - ax) / m
+        pt[1] = (pt[1] - ay) / m
+        pt[2] = (pt[2] - az) / m
+    
+    scale = m * 10
+    
     # game loop
     while running:
         clock.tick(fps)
@@ -109,7 +141,6 @@ def start_game():
             # moving view events
             if not move_view and event.type == pygame.MOUSEBUTTONDOWN:
                 move_view = True
-                mx = pygame.mouse.get_pos()
             elif move_view and event.type == pygame.MOUSEBUTTONUP:
                 move_view = False
             if move_view:
@@ -119,14 +150,16 @@ def start_game():
                 m_pos = cm_pos
 
         # project points from 3d to 2d
-        proj_verts = project(angle_x, angle_y, cube_vertices, cube_center, scale)
-        #proj_verts = project(angle_x, angle_y, sim_pts, cube_center, scale)
+        proj_verts = project(angle_x, angle_y, sim_pts, cube_center, scale)
+        #proj_verts = project(angle_x, angle_y, sim_pts, proj_center, scale)
 
         # draw vertices as circles:
         for p in proj_verts:
-            pygame.draw.circle(screen, circle_color, (p[0], p[1]), 8)
-
+            if p[0] > 0 and p[1] > 0 and p[0] < sc_width and p[1] < sc_height:
+                pygame.draw.circle(screen, circle_color, (p[0], p[1]), 8)
+        
         # draw cube sides:
+        '''
         counter = 1
         for side in cube_sides:
             side_coords = [
@@ -139,7 +172,8 @@ def start_game():
                 int(shape[0] * counter * 0.05), int(shape[1] * counter * 0.05),
                 int(shape[2] * counter * 0.05)), side_coords)
             counter += 1
-
+            '''
+            
         # present the new image to screen:
         if not pause:
             pygame.display.update()
